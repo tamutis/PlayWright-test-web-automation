89cd055baccd9e0f52d016a5ab3f762892dafea5
AndreTamutis <tamutisf@gmail.com>
2023-07-28 20:03:07 -0300

    criação do projeto

---
+++ tests/specs/tasks.spec.ts
@@ -0,0 +1,62 @@
+import { test, expect } from '@playwright/test';
+import { TasksPage } from '../support/pages/tasksPages';
+import { taskModel } from '../fixture/task.model';
+import data from '/Users/macintosh/Documents/Mottu/test-web-automation/tests/fixture/tasks.json'
+
+let tasksPage: TasksPage
+
+test.beforeEach(({ page }) => {
+  tasksPage = new TasksPage(page)
+})
+
+test.describe('Cadastro', () => {
+  test('Deve poder cadastrar uma tarefa', async ({ }) => {
+    const task = data.success as taskModel
+    await tasksPage.go();
+    await tasksPage.create(task);
+    await tasksPage.assertTaskVisible(task.name)
+    await tasksPage.toggle(task.name)
+    await tasksPage.assertdBeDone(task.name)
+  })
+});
+
+test.describe('Validação', () => {
+  test('Deve retornar tarefa selecionada', async ({ }) => {
+    const task = data.success as taskModel
+    const tasks = data.completed as taskModel
+
+    await tasksPage.go();
+    await tasksPage.create(task);
+    await tasksPage.create(tasks);
+    await tasksPage.toggle(task.name)
+    await tasksPage.btnCompleted();
+    await tasksPage.assertTaskVisible(task.name)
+  })
+
+  test('Deve retornar tarefa não selecionada', async ({ }) => {
+    const task = data.success as taskModel
+    const tasks = data.active as taskModel
+
+    await tasksPage.go();
+    await tasksPage.create(task);
+    await tasksPage.create(tasks);
+    await tasksPage.toggle(task.name)
+    await tasksPage.btnActive();
+    await tasksPage.assertTaskVisible(tasks.name)
+  })
+});
+
+test.describe('Deletar', () => {
+  test('Deletar tarefa selecionada', async ({ }) => {
+    const task = data.success as taskModel
+    const tasks = data.completed as taskModel
+
+    await tasksPage.go();
+    await tasksPage.create(task);
+    await tasksPage.create(tasks);
+    await tasksPage.toggle(task.name)
+    await tasksPage.btnCompleted();
+    await tasksPage.btnClearCompleted();
+    await tasksPage.assertNotTaskVisible(false);
+  })
+});
